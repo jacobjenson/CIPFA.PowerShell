b9b2035b55826fb4a11973f97d14411eaf4a424c
jacobjenson <jacobjenson@outlook.com>
2025-05-09 13:23:36 +0100

    Initial Commit

---
+++ CopyFiles.ps1
@@ -0,0 +1,41 @@
+# Views
+# Copy-Item "C:\Source\CIPFA.org\Product\www\Views\StudentRegistrationD365\Register.cshtml" -Destination "C:\inetpub\wwwroot\cipfacm.dev.local\Views\StudentRegistrationD365\Register.cshtml"
+# Copy-Item "C:\Source\CIPFA.org\Product\www\Views\StudentRegistrationD365\_Requirements.cshtml" -Destination "C:\inetpub\wwwroot\cipfacm.dev.local\Views\StudentRegistrationD365\_Requirements.cshtml"
+# Copy-Item "C:\Source\CIPFA.org\Product\www\Views\StudentRegistrationD365\_PersonalDetails.cshtml" -Destination "C:\inetpub\wwwroot\cipfacm.dev.local\Views\StudentRegistrationD365\_PersonalDetails.cshtml"
+# Copy-Item "C:\Source\CIPFA.org\Product\www\Views\StudentRegistrationD365\_QualificationsToStudy.cshtml" -Destination "C:\inetpub\wwwroot\cipfacm.dev.local\Views\StudentRegistrationD365\_QualificationsToStudy.cshtml"
+# Copy-Item "C:\Source\CIPFA.org\Product\www\Views\StudentRegistrationD365\_ContactDetails.cshtml" -Destination "C:\inetpub\wwwroot\cipfacm.dev.local\Views\StudentRegistrationD365\_ContactDetails.cshtml"
+Copy-Item "C:\Source\CIPFA.org\Product\www\Views\StudentRegistrationD365\_BusinessDetails.cshtml" -Destination "C:\inetpub\wwwroot\cipfacm.dev.local\Views\StudentRegistrationD365\_BusinessDetails.cshtml"
+Copy-Item "C:\Source\CIPFA.org\Product\www\Views\StudentRegistrationD365\_EntryRoute.cshtml" -Destination "C:\inetpub\wwwroot\cipfacm.dev.local\Views\StudentRegistrationD365\_EntryRoute.cshtml"
+# Copy-Item "C:\Source\CIPFA.org\Product\www\Views\StudentRegistrationD365\_ProfessionalQualifictions.cshtml" -Destination "C:\inetpub\wwwroot\cipfacm.dev.local\Views\StudentRegistrationD365\_ProfessionalQualifictions.cshtml"
+# Copy-Item "C:\Source\CIPFA.org\Product\www\Views\StudentRegistrationD365\_Declaration.cshtml" -Destination "C:\inetpub\wwwroot\cipfacm.dev.local\Views\StudentRegistrationD365\_Declaration.cshtml"
+# Copy-Item "C:\Source\CIPFA.org\Product\www\Views\StudentRegistrationD365\_DeclarationModal.cshtml" -Destination "C:\inetpub\wwwroot\cipfacm.dev.local\Views\StudentRegistrationD365\_DeclarationModal.cshtml"
+# Copy-Item "C:\Source\CIPFA.org\Product\www\Views\StudentRegistrationD365\_AcademicQualifictions.cshtml" -Destination "C:\inetpub\wwwroot\cipfacm.dev.local\Views\StudentRegistrationD365\_AcademicQualifictions.cshtml"
+
+# Other Views
+# Copy-Item "C:\Source\CIPFA.org\Product\www\Views\Registration\Register.cshtml" -Destination "C:\inetpub\wwwroot\cipfacm.dev.local\Views\Registration\Register.cshtml"
+# Copy-Item "C:\Source\CIPFA.org\Product\www\Views\Shared\_2020Layout.cshtml" -Destination "C:\inetpub\wwwroot\cipfacm.dev.local\Views\Shared\_2020Layout.cshtml"
+
+# Editor Tempalates
+# Copy-Item "C:\Source\CIPFA.org\Product\www\Views\Shared\EditorTemplates\ColumnTextBox.cshtml" -Destination "C:\inetpub\wwwroot\cipfacm.dev.local\Views\Shared\EditorTemplates\ColumnTextBox.cshtml"
+# Copy-Item "C:\Source\CIPFA.org\Product\www\Views\Shared\EditorTemplates\ColumnDropDownList.cshtml" -Destination "C:\inetpub\wwwroot\cipfacm.dev.local\Views\Shared\EditorTemplates\ColumnDropDownList.cshtml"
+# Copy-Item "C:\Source\CIPFA.org\Product\www\Views\Shared\EditorTemplates\ColumnCheckbox.cshtml" -Destination "C:\inetpub\wwwroot\cipfacm.dev.local\Views\Shared\EditorTemplates\ColumnCheckbox.cshtml"
+# Copy-Item "C:\Source\CIPFA.org\Product\www\Views\Shared\EditorTemplates\ColumnDate.cshtml" -Destination "C:\inetpub\wwwroot\cipfacm.dev.local\Views\Shared\EditorTemplates\ColumnDate.cshtml"
+# Copy-Item "C:\Source\CIPFA.org\Product\www\Views\Shared\EditorTemplates\ColumnFile.cshtml" -Destination "C:\inetpub\wwwroot\cipfacm.dev.local\Views\Shared\EditorTemplates\ColumnFile.cshtml"
+# Copy-Item "C:\Source\CIPFA.org\Product\www\Views\Shared\EditorTemplates\ColumnTextArea.cshtml" -Destination "C:\inetpub\wwwroot\cipfacm.dev.local\Views\Shared\EditorTemplates\ColumnTextArea.cshtml"
+
+# Layout
+# Copy-Item "C:\Source\CIPFA.org\Product\www\Views\Shared\_Layout.cshtml" -Destination "C:\inetpub\wwwroot\cipfacm.dev.local\Views\Shared\_Layout.cshtml"
+
+# css
+# Copy-Item "C:\Source\CIPFA.org\Product\www\css\sass\cipfa.css" -Destination "C:\inetpub\wwwroot\cipfacm.dev.local\css\sass\cipfa.css"
+# Copy-Item "C:\Source\CIPFA.org\Product\www\css\sass\cipfa.min.css" -Destination "C:\inetpub\wwwroot\cipfacm.dev.local\css\sass\cipfa.min.css"
+
+# JS
+# Copy-Item "C:\Source\CIPFA.org\Product\www\js\D365\StudentRegister.js" -Destination "C:\inetpub\wwwroot\cipfacm.dev.local\js\D365\StudentRegister.js"
+# Copy-Item "C:\Source\CIPFA.org\Product\www\Scripts\jquery.validate.additional.js" -Destination "C:\inetpub\wwwroot\cipfacm.dev.local\Scripts\jquery.validate.additional.js"
+# Copy-Item "C:\Source\CIPFA.org\Product\www\Scripts\bootstrap-select.js" -Destination "C:\inetpub\wwwroot\cipfacm.dev.local\Scripts\bootstrap-select.js"
+# Copy-Item "C:\Source\CIPFA.org\Product\www\Scripts\bootstrap-select.min.js" -Destination "C:\inetpub\wwwroot\cipfacm.dev.local\Scripts\bootstrap-select.min.js"
+# Copy-Item "C:\Source\CIPFA.org\Product\www\js\choices\choices.min.js" -Destination "C:\inetpub\wwwroot\cipfacm.dev.local\js\choices\choices.min.js"
+
+# Config
+# Copy-Item "C:\Source\CIPFA.org\Product\www\App_Config\Include\Project\Cipfa.Settings.config" -Destination "C:\inetpub\wwwroot\cipfacm.dev.local\App_Config\Include\Project\Cipfa.Settings.config"
